f5ab2db3020c262f406537891bcaae8c31e2313a
Vladimir <boooobka@gmail.com>
2017-09-18 22:17:30 +0400

    clean code

--- src/ElementGrid.js
+++ src/ElementGrid.js
@@ -1,73 +1,82 @@
+import Element from './Element';
 class ElementGrid{
+
   constructor(aSize){
-    let hash = {}; 
-    let size = aSize;     
-  }  
+    this.hash = {}; 
+    this.size = aSize;     
+  }; 
+
   init(){  
-  let xPos = 0;
-  let yPos = 0; 
-  let temp;
-  for(let i = 0; i < Math.pow(this.size, 2); i++){  
+    let xPos = 0;
+    let yPos = 0; 
+    let temp;
+    for(let i = 0; i < Math.pow(this.size, 2); i++){  
       temp = this.indexToPosition(i);
       xPos = temp.X;
       yPos = temp.Y;
       this.hash[xPos + "" + yPos] = new Element(false, xPos, yPos);      
-  }   
- }
+    }   
+  };
 
- indexToPosition(index){
-   let yFind = parseInt(index / 10);
-   let xFind = index - yFind * 10;
-     return {X: xFind, Y: yFind};
- } 
- findElement(obj, posX, posY){
-    return obj[posX + "" + posY];
- }
- 
- getNextX(obj, posX, posY){  
-   let nextX = (posX === 9) ? 0 : posX + 1;
-   return obj[nextX + " " + posY];
- }
- 
- getPrevX(obj, posX, posY){
-   let prevX = (posX === 0) ? 9 : posX - 1;
-   return obj[prevX + " " + posY];
- }
+  addTransform(){
+  };
 
- getNextY(obj, posX, posY){
-   let nextY = (posY === 9) ? 0 : posY + 1;
-   return obj[posX + " " + nextY];
- }
+  allDie(){
+  };
  
- getPrevY(obj, posX, posY){
-   let prevY = (posY === 0) ? 9 : posY - 1;
-   return obj[posX + " " + prevY];
- }
+  indexToPosition(index){
+    let yFind = parseInt(index / 10);
+    let xFind = index - yFind * 10;
+    return {X: xFind, Y: yFind};
+  };
+
+  findElement(posX, posY){
+    return this.hash[posX + "" + posY];
+  };
  
- getNextXNextY(obj, posX, posY){
-   let nextX = (posX === 9) ? 0 : posX + 1; 
-   let nextY = (posY === 9) ? 0 : posY + 1; 
-   return obj[nextX + " " + nextY];
- }
-
- getNextXPrevY(obj, posX, posY){
-   let nextX = (posX === 9) ? 0 : posX + 1;
-   let prevY = (posY === 0) ? 9 : posY - 1;
-   return obj[nextX + " " + prevY];
- }
+  getNextX(posX, posY){  
+    let nextX = (posX === 9) ? 0 : posX + 1;
+    return this.hash[nextX + " " + posY];
+  };
  
- getPrevXNextY(obj, posX, posY){
-   let prevX = (posX === 0) ? 9 : posX - 1;
-   let nextY = (posY === 9) ? 0 : posY + 1;
-   return obj[prevX + " " + nextY];
- }
+  getPrevX(posX, posY){
+    let prevX = (posX === 0) ? 9 : posX - 1;
+    return this.hash[prevX + " " + posY];
+  };
 
- getPrevXPrevY(obj, posX, posY){
-   let prevX = (posX === 0) ? 9 : posX - 1;
-   let prevY = (posY === 0) ? 9 : posY - 1;
-  return obj[prevX + " " + prevY];
- }
+  getNextY(posX, posY){
+    let nextY = (posY === 9) ? 0 : posY + 1;
+    return this.hash[posX + " " + nextY];
+  };
+ 
+  getPrevY(posX, posY){
+    let prevY = (posY === 0) ? 9 : posY - 1;
+    return this.hash[posX + " " + prevY];
+  };
+ 
+  getNextXNextY(posX, posY){
+    let nextX = (posX === 9) ? 0 : posX + 1; 
+    let nextY = (posY === 9) ? 0 : posY + 1; 
+    return this.hash[nextX + " " + nextY];
+  };
 
+  getNextXPrevY(posX, posY){
+    let nextX = (posX === 9) ? 0 : posX + 1;
+    let prevY = (posY === 0) ? 9 : posY - 1;
+    return this.hash[nextX + " " + prevY];
+  };
+ 
+  getPrevXNextY(posX, posY){
+    let prevX = (posX === 0) ? 9 : posX - 1;
+    let nextY = (posY === 9) ? 0 : posY + 1;
+    return this.hash[prevX + " " + nextY];
+  };
+
+  getPrevXPrevY(posX, posY){
+    let prevX = (posX === 0) ? 9 : posX - 1;
+    let prevY = (posY === 0) ? 9 : posY - 1;
+    return this.hash[prevX + " " + prevY];
+  };
 }
 
 export default ElementGrid;
