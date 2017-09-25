84f3dc2011220395ae2d31d66d67f9383673b9b4
Vladimir <boooobka@gmail.com>
2017-09-25 21:30:55 +0400

    delete unsed props value

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 class Button extends Component {
   render() {
     return (
-      <div className ={`button ${ this.props.value === "Start" ? "green" : "" }`}    onClick={this.props.start}>
+      <div className = "button green" onClick={this.props.start}>
       {this.props.value}       
       </div>
     );
